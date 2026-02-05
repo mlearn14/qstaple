8cab6d74ce5af80a55bf837f6db0cbe2f75482b4
mlearn14 <matthew.learn@stonybrook.edu>
2026-02-05 14:58:14 -0500

    init

---
+++ dist/qstaple.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Always run from this script's directory
+cd "$(dirname "$0")"
+
+./qstaple
---
+++ qstaple.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import os
+from pathlib import Path
+import sys
+
+from pypdf import PdfWriter
+
+
+def main(dir):
+    dir = Path(dir).expanduser()
+
+    if not dir.is_dir():
+        print(f"Directory {dir} does not exist")
+        sys.exit(1)
+
+    pdfs = sorted(dir.glob("*.pdf"))
+    print(pdfs)
+
+    if not pdfs:
+        print("No PDF files found.")
+        return
+
+    output = os.path.join(dir, "deployment_packet.pdf")
+
+    if Path(output) in pdfs:
+        print(
+            f"Output file already exists. Delete {output} before proceding further. Exiting."
+        )
+        sys.exit(1)
+
+    merger = PdfWriter()
+    for pdf in pdfs:
+        print(f"Adding {pdf.name}")
+        merger.append(str(pdf))
+
+    merger.write(output)
+    merger.close()
+
+    print(f"\nCreated: {output}")
+
+
+if __name__ == "__main__":
+    directory = input("Enter directory to staple PDFs from:\n> ").strip()
+    main(directory)
+    input("\nPress any key to close...")
